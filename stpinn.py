dffcb4cb012867029b3dce19c07f9000c1dc3a45
ronsperber <85753940+ronsperber@users.noreply.github.com>
2025-10-30 14:11:17 -0400

    Update stpinn.py

--- stpinn.py
+++ stpinn.py
@@ -23,7 +23,7 @@ with st.expander("Expand to see the mathematics behind this method.", expanded=F
 ode_choice = st.sidebar.selectbox("Choose ODE", list(ODES.keys()))
 
 
-on_streamlit_cloud = "ST_STREAMLIT_CLOUD" in os.environ
+on_streamlit_cloud = "STREAMLIT_SERVER_ENABLE_XSRF_PROTECTION" in os.environ
 if on_streamlit_cloud:
     make_gif = st.sidebar.checkbox(
         "Generate animation GIF (disabled on Streamlit Cloud)",
